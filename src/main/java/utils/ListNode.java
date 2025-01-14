1b2b8e6b8ea0cfe438cdd99b04f9203ed84b2756
PC <user985546@gmail.com>
2025-01-14 10:59:53 +0100

    Solutions for more problems in "popular" section #4

--- src/main/java/utils/ListNode.java
+++ src/main/java/utils/ListNode.java
@@ -15,4 +15,27 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public static ListNode createList(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for (int num : arr) {
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+
+    }
 }
